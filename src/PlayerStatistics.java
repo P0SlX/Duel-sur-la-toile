8ad53c9cfa684e3373e424eac244a67e8d66d4b7
Theojc <jehennecousty@gmail.com>
2020-06-08 16:09:02 +0200

    Ajout de la classe PlayerStatistics

---
+++ src/PlayerStatistics.java
@@ -0,0 +1,37 @@
+public class PlayerSatistics {
+
+    private int playedGames;
+    private int winnedGames;
+    private double turnPlayedPerGamesAverage;
+    private int activeGames;
+    private int abandonedGames;
+
+    public PlayerSatistics(int playedGames, int winnedGames, double turnPlayedPerGamesAverage, int activeGames, int abandonedGames){
+        this.playedGames=playedGames;
+        this.winnedGames=winnedGames;
+        this.turnPlayedPerGamesAverage=turnPlayedPerGamesAverage;
+        this.activeGames=activeGames;
+        this.abandonedGames=abandonedGames;
+
+    }
+
+    public int getPlayedGames() {
+        return playedGames;
+    }
+
+    public int getWinnedGames() {
+        return winnedGames;
+    }
+
+    public double getTurnPlayedPerGamesAverage() {
+        return turnPlayedPerGamesAverage;
+    }
+
+    public int getActiveGames() {
+        return activeGames;
+    }
+
+    public int getAbandonedGames() {
+        return abandonedGames;
+    }
+}
