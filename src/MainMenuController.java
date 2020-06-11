6131063adc247adade1c3485c3dbc54f2e9afb53
Corentin <coco41.cm@gmail.com>
2020-06-11 12:25:46 +0200

    Ajout du chargement des messages

--- src/MainMenuController.java
+++ src/MainMenuController.java
@@ -1,4 +1,7 @@
 import com.gluonhq.charm.glisten.control.Avatar;
+import javafx.css.CssParser;
+import javafx.css.Rule;
+import javafx.css.Stylesheet;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
@@ -7,9 +10,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.Separator;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 import java.net.URL;
@@ -18,11 +20,6 @@ import java.util.ResourceBundle;
 
 public class MainMenuController extends Controller implements Initializable {
 
-    private final String PSEUDO = "-fx-font-size: 13pt; -fx-font-family: \"Segoe UI Light\";" +
-                                  "-fx-text-fill: white;" +
-                                  "-fx-opacity: 1;";
-
-    private final String BUTTON = "-fx-background-radius: 5px;";
     @FXML
     private MenuItem disconnect;
 
@@ -37,6 +34,9 @@ public class MainMenuController extends Controller implements Initializable {
 
     @FXML
     private Label ratio;
+
+    @FXML
+    private VBox messageList;
 
     private Player loggedPlayer;
 
@@ -81,6 +81,9 @@ public class MainMenuController extends Controller implements Initializable {
         friendPseudo.setLayoutY(48.0);
         friendPseudo.prefHeight(21.0);
         friendPseudo.prefWidth(109.0);
+        String PSEUDO = "-fx-font-size: 13pt; -fx-font-family: \"Segoe UI Light\";" +
+                "-fx-text-fill: white;" +
+                "-fx-opacity: 1;";
         friendPseudo.setStyle(PSEUDO);
 
         friendRatio.setAlignment(Pos.CENTER_RIGHT);
@@ -92,6 +95,7 @@ public class MainMenuController extends Controller implements Initializable {
         invite.setMnemonicParsing(false);
         invite.prefHeight(32.0);
         invite.prefWidth(71.0);
+        String BUTTON = "-fx-background-radius: 5px;";
         invite.setStyle(BUTTON);
 
         message.setLayoutX(15.0);
@@ -100,6 +104,8 @@ public class MainMenuController extends Controller implements Initializable {
         message.prefHeight(32.0);
         message.prefWidth(71.0);
         message.setStyle(BUTTON);
+
+        message.setOnAction(actionEvent -> loadMessage(friend));
 
         avatar.setLayoutX(206.0);
         avatar.setLayoutY(6.0);
@@ -115,4 +121,50 @@ public class MainMenuController extends Controller implements Initializable {
         friendList.getChildren().add(separator);
     }
 
+    private void loadMessage(Player sender) {
+        ArrayList<Message> messages = databaseConnection.getPlayerMessage(this.loggedPlayer, sender);
+
+        for(Message m : messages) {
+            HBox messageVBox = new HBox();
+            messageVBox.prefWidth(250.0);
+
+            VBox content = new VBox();
+            content.setAlignment(Pos.CENTER);
+            content.maxHeight(1.7976931348623157E308);
+            content.maxWidth(1.7976931348623157E308);
+            content.minHeight(Double.NEGATIVE_INFINITY);
+            content.minWidth(Double.NEGATIVE_INFINITY);
+
+            Label pseudo = new Label(sender.getPseudo());
+            Label time = new Label(m.getDate());
+
+            setMessageLabelStyle(pseudo);
+            setMessageLabelStyle(time);
+            content.getChildren().addAll(pseudo, time);
+            messageVBox.getChildren().add(content);
+
+            Label messageContent = new Label(m.getContent());
+            setMessageLabelStyle(messageContent);
+            messageContent.setPrefWidth(175.0);
+            messageVBox.getChildren().add(messageContent);
+
+            this.messageList.getChildren().add(messageVBox);
+        }
+    }
+
+    private void setMessageLabelStyle(Label label) {
+        label.setAlignment(Pos.CENTER);
+        label.setMaxHeight(1.7976931348623157E308);
+        label.setMaxWidth(1.7976931348623157E308);
+        label.setMinHeight(Double.NEGATIVE_INFINITY);
+        label.setMinWidth(Double.NEGATIVE_INFINITY);
+        label.setPrefWidth(75.0);
+        String LABEL_BRIGHT = "-fx-font-size: 11pt;" +
+                "-fx-font-family: \"Segoe UI Light\";" +
+                "-fx-text-fill: white;" +
+                "-fx-opacity: 1;";
+        label.setStyle(LABEL_BRIGHT);
+    }
 }
+
+
