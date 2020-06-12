721f99abb5369817b60152e532e557437fdd77ff
Thibout <jordan.thibout@etu.univ-orleans.fr>
2020-06-12 16:40:27 +0200

    correction bug ds adminStatisticsTest

--- tests/AdminStatisticsTest.java
+++ tests/AdminStatisticsTest.java
@@ -2,19 +2,19 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AdminStatisticsTest {
-    @test
+    @Test
     void getGamesLaunchedPerHour() {
         AdminStatistics as = new AdminStatistics(10, 20 ,30);
         assertEquals(as.getGamesLaunchedPerHour(), 10.0);
     }
 
-    @test
+    @Test
     void getGetGamesLaunchedPerDay() {
         AdminStatistics as = new AdminStatistics(10, 20 ,30);
         assertEquals(as.getGetGamesLaunchedPerDay(), 20.0);
     }
 
-    @test
+    @Test
     void getGamesLaunchedPerMonth() {
         AdminStatistics as = new AdminStatistics(10, 20 ,30);
         assertEquals(as.getGamesLaunchedPerMonth(), 30.0);
