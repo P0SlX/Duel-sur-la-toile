9ea9e71d0e2750bc4442cfbcf40da30e50b29ecc
anatole <anatole.boisserie@etu.univ-orleans.fr>
2020-06-12 17:02:07 +0200

    correction adminstatisticstests

--- tests/AdminStatisticsTest.java
+++ tests/AdminStatisticsTest.java
@@ -1,10 +1,6 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 721f99abb5369817b60152e532e557437fdd77ff
 class AdminStatisticsTest {
     @Test
     void getGamesLaunchedPerHour() {
@@ -18,10 +14,6 @@ class AdminStatisticsTest {
         assertEquals(as.getGetGamesLaunchedPerDay(), 20.0);
     }
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 721f99abb5369817b60152e532e557437fdd77ff
     @Test
     void getGamesLaunchedPerMonth() {
         AdminStatistics as = new AdminStatistics(10, 20 ,30);
