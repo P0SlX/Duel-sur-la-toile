75afabf5c0a101636d45571f8e016ed49eaf6a1e
Theojc <jehennecousty@gmail.com>
2020-06-15 17:38:55 +0200

    Javadoc de FriendInvitation

--- src/FriendInvitation.java
+++ src/FriendInvitation.java
@@ -16,49 +16,63 @@ public class FriendInvitation implements Invitation {
 
     @Override
     public Player getSender() {
-        // Return a Player which is the sender of the invitation
+        /**
+         * @return PLayer, the sender of the invitation
+         */
 
         return this.emitter;
     }
 
     @Override
     public Player getReceiver() {
-        // Return a Player which is the receiver of the invitation
+        /**
+         * @return Player, the receiver of the invitation
+         */
 
         return this.receiver;
     }
 
     @Override
     public java.sql.Date getEmissionDate() {
-        // Return a Date which is the sending date
+        /**
+         * @return Date, the date of sending
+         */
 
         return emissionDate;
     }
 
     @Override
     public int getEtatInv() {
-        // Return an int which is the state of the invitation (-1, 0 or 1)
+        /**
+         * @return int, the state of the invitation (-1, 0 or 1)
+         */
 
         return this.etatInv;
     }
 
     @Override
     public int getId() {
-        // Return an int which is the id of the invitation
+        /**
+         * @return int, the id of the invitation
+         */
 
         return this.id;
     }
 
     @Override
     public void accept() {
-        // Change the state of the invitation to 1
+        /**
+         * Change the state of the invitation to 1
+         */
 
         this.etatInv = ACCEPTED;
     }
 
     @Override
     public void decline() {
-        // Change the state of the invitation to -1
+        /**
+         * Change the state of the invitation to -1
+         */
 
         this.etatInv = REFUSED;
     }
