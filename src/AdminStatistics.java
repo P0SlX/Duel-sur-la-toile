5d38455e4da0c6110fe20dfdeb9577546a079013
anatole <anatole.boisserie@etu.univ-orleans.fr>
2020-06-15 14:45:30 +0200

    javadoc adminStatistics

--- src/AdminStatistics.java
+++ src/AdminStatistics.java
@@ -3,20 +3,35 @@ public class AdminStatistics {
     private double getGamesLaunchedPerDay;
     private double gamesLaunchedPerMonth;
 
+    /**
+     * Builder of adminStatistics
+     * @param gamesLaunchedPerHour double, the number of games launched per hour by the application
+     * @param getGamesLaunchedPerDay double, the number of games launched per day by the application
+     * @param gamesLaunchedPerMonth double, the number of games launched per month by the application
+     */
     public AdminStatistics(double gamesLaunchedPerHour, double getGamesLaunchedPerDay, double gamesLaunchedPerMonth) {
         this.gamesLaunchedPerHour = gamesLaunchedPerHour;
         this.getGamesLaunchedPerDay = getGamesLaunchedPerDay;
         this.gamesLaunchedPerMonth = gamesLaunchedPerMonth;
     }
 
+    /**
+     * @return double, the number of games launched per hour by the application
+     */
     public double getGamesLaunchedPerHour() {
         return gamesLaunchedPerHour;
     }
 
+    /**
+     * @return double, the number of games launched per day by the application
+     */
     public double getGetGamesLaunchedPerDay() {
         return getGamesLaunchedPerDay;
     }
 
+    /**
+     * @return double, the number of games launched per month by the application
+     */
     public double getGamesLaunchedPerMonth() {
         return gamesLaunchedPerMonth;
     }
