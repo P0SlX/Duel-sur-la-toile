41031824975b06830cef185b5555341b55985ac1
Corentin <coco41.cm@gmail.com>
2020-06-16 12:58:23 +0200

    Ajout de la deconnexion et du retour au menu principal

--- src/InvitationController.java
+++ src/InvitationController.java
@@ -1,21 +1,32 @@
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 public class InvitationController extends Controller implements Initializable {
+
+    @FXML
+    private Label pseudo;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
     }
 
     @FXML
-    public void onDisconnectAction(ActionEvent actionEvent) {
+    public void onDisconnectAction() throws SQLException {
+        databaseConnection.setStatus(loggedPlayer, Player.DISCONNECTED);
     }
 
     @FXML
-    public void onBackHomeAction(ActionEvent actionEvent) {
+    public void onBackHomeAction() {
+        sceneController.showScene(SceneController.ViewType.MainMenu);
+    }
+
+    public void initInvitationController() {
+
     }
 }
