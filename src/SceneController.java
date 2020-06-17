418ac44f7e777595083548be6fd07b9e4438a94d
Theojc <jehennecousty@gmail.com>
2020-06-17 15:40:39 +0200

    Ajout Javadoc SceneController

--- src/SceneController.java
+++ src/SceneController.java
@@ -18,18 +18,36 @@ public class SceneController {
     }
 
     public void addScene(ViewType type, Pane scene) {
+        /**
+         * Add a scene and it type in this.views
+         * @param type ViewType, the view type...
+         * @param scene Pane, the new scene
+         */
         views.put(type, scene);
     }
 
     public void showScene(ViewType type) {
+        /**
+         * Show a scene on the mainScene (contained in this.views)
+         * @param type ViewType, the type of the scene
+         */
         mainScene.setRoot(views.get(type));
     }
 
     public Pane getScene(ViewType type) {
+        /**
+         * get a scene in this.views
+         * @param type ViewType, the type of the scene
+         * @return Pane, the seeked scene
+         */
         return views.get(type);
     }
 
     public HashMap<ViewType, Pane> getViews() {
+        /**
+         * get all the scenes
+         * @return HashMap<ViewType, Pane>, simply this.views
+         */
         return views;
     }
 }
